1df56c84249b6d37ad733df57fa34cd72ac69db9
arthurlormeauline <arthur.lormeau@protectline.fr>
2025-08-27 16:38:11 +0200

    JsBpmnHelper add json persistence to block

---
+++ src/main/java/com/protectline/common/block/jsonblock/FunctionJsonBlockUtil.java
@@ -0,0 +1,31 @@
+package com.protectline.common.block.jsonblock;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.protectline.common.block.FunctionBlock;
+
+import java.io.IOException;
+import java.nio.file.Path;
+
+public class FunctionJsonBlockUtil {
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+
+    public static void writeToFile(FunctionBlock functionBlock, Path filePath) throws IOException {
+        FunctionJsonBlock jsonBlock = new FunctionJsonBlock(functionBlock);
+        objectMapper.writeValue(filePath.toFile(), jsonBlock);
+    }
+
+    public static FunctionBlock readFromFile(Path filePath) throws IOException {
+        FunctionJsonBlock jsonBlock = objectMapper.readValue(filePath.toFile(), FunctionJsonBlock.class);
+        return jsonBlock.toFunctionBlock();
+    }
+
+    public static String toJsonString(FunctionBlock functionBlock) throws IOException {
+        FunctionJsonBlock jsonBlock = new FunctionJsonBlock(functionBlock);
+        return objectMapper.writeValueAsString(jsonBlock);
+    }
+
+    public static FunctionBlock fromJsonString(String jsonString) throws IOException {
+        FunctionJsonBlock jsonBlock = objectMapper.readValue(jsonString, FunctionJsonBlock.class);
+        return jsonBlock.toFunctionBlock();
+    }
+}

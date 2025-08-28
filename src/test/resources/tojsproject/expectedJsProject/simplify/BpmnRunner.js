2572c3bd96e7b0bb5a9081d82bde16a3fe7903c0
arthurlormeauline <arthur.lormeau@protectline.fr>
2025-08-28 11:30:42 +0200

    JsBpmnHelper implement expected js project for simplify.bpmn

--- src/test/resources/tojsproject/expectedJsProject/simplify/BpmnRunner.js
+++ src/test/resources/tojsproject/expectedJsProject/simplify/BpmnRunner.js
@@ -62,7 +62,34 @@ class BpmnRunner {
         console.log(message);
     }
 
-    // Les méthodes générées seront ajoutées ici
+    //<BT:MAIN>
+
+    //<880ed997-78b2-4ff6-bb02-9eeadfa6bcb7>
+    Delay_definition_0(){
+        delay definition script
+    }
+    //<880ed997-78b2-4ff6-bb02-9eeadfa6bcb7/>
+
+    //<74b70353-a19e-4ad7-9838-5251ab67d8a6>
+    Get_device_by_name_0(){
+        get device by name url script
+    }
+    //<74b70353-a19e-4ad7-9838-5251ab67d8a6/>
+
+    //<a96073f8-befb-430d-b8a0-466100ca5bc9>
+    Get_device_by_name_1(){
+        get device by name output script
+    }
+    //<a96073f8-befb-430d-b8a0-466100ca5bc9/>
+
+
+    //<da508040-ebc4-4267-82fb-d7d4576e136e>
+    Event_1u1d1qi_0(){
+        start event script
+    }
+    //<da508040-ebc4-4267-82fb-d7d4576e136e/>
+
+    //<BT:MAIN/>
 }
 
 module.exports = BpmnRunner;

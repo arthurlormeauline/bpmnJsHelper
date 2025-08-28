cb8b5877715ac16e7ac64cb6bbdbb42ccf99cfa6
arthurlormeauline <arthur.lormeau@protectline.fr>
2025-08-28 09:35:58 +0200

    JsBpmnHelper put workingDirectory in constructor

--- src/main/java/com/protectline/application/tobpmn/JsProjectToBpmn.java
+++ src/main/java/com/protectline/application/tobpmn/JsProjectToBpmn.java
@@ -7,18 +7,16 @@ import java.io.IOException;
 import java.nio.file.Path;
 
 public class JsProjectToBpmn {
-    private final Path workingDirectory;
     private final FromJsProjectToBlocks jsToBlocks;
     private final FromBlockToBpmn blocksToBpmn;
 
     public JsProjectToBpmn(Path workingDirectory) {
-        this.workingDirectory = workingDirectory;
         this.jsToBlocks = new FromJsProjectToBlocks(workingDirectory);
-        this.blocksToBpmn = new FromBlockToBpmn();
+        this.blocksToBpmn = new FromBlockToBpmn(workingDirectory);
     }
 
     public void updateBpmn(String process) throws IOException {
         jsToBlocks.createBlocksFromJsProject();
-        blocksToBpmn.updateBpmnFromBlocks(workingDirectory,process);
+        blocksToBpmn.updateBpmnFromBlocks(process);
     }
 }

088aa0a0f33f4de1af0f748a23543e54aab87dec
arthurlormeauline <arthur.lormeau@protectline.fr>
2025-09-02 11:33:12 +0200

    JsBpmnHelper clean

--- xml-parser/src/main/java/com/protectline/bpmninjs/xmlparser/XmlParser.java
+++ xml-parser/src/main/java/com/protectline/bpmninjs/xmlparser/XmlParser.java
@@ -12,16 +12,6 @@ public class XmlParser {
 
     public List<Element> parseXml(String jsContent, TokenDefinition tokenDefinition){
         List<Token> tokens = new Lexer(tokenDefinition).tokenize(jsContent);
-        System.out.println("DEBUG XmlParser: Lexer found " + tokens.size() + " tokens");
-        List<Element> elements = tokenParser.parseTokensToElements(tokens);
-        System.out.println("DEBUG XmlParser: TokenParser created " + elements.size() + " elements");
-        for (int i = 0; i < elements.size(); i++) {
-            Element element = elements.get(i);
-            System.out.println("DEBUG XmlParser: Element " + i + ": '" + element.getElementName() + 
-                             "' with attributes: " + element.getAttributes() +
-                             " and content length: " + (element.getContent() != null ? element.getContent().length() : "null"));
-        }
-        
-        return elements;
+        return tokenParser.parseTokensToElements(tokens);
     }
 }
